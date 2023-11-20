0521630dade8f2c40cc169b45e8e47a735f5c4fc
bhanu6222 <bhanu6222@gmail.com>
2023-11-20 16:25:33 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './nischal.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <br></br>
+        <br></br>
+          Welcome to <b>prestine Apertures</b>, the ultimate porn productions
+          <br></br>
+          <b>No body, no body, no body does it better</b>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
